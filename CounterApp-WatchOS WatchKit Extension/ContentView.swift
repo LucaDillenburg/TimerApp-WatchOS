4d91d405106f1d58ac6ee4f8e61aefb630ccdf42
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-02-16 00:33:19 -0300

    Create timer app

--- CounterApp-WatchOS WatchKit Extension/ContentView.swift
+++ CounterApp-WatchOS WatchKit Extension/ContentView.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var secondScreenShown = false
+    @State var timerVal = 5
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack(alignment: .center) {
+            Text("Start timer for")
+            Picker(selection: $timerVal, label: Text("")) {
+                Text("5 seconds").tag(5)
+                Text("10 seconds").tag(10)
+                Text("15 seconds").tag(15)
+                Text("20 seconds").tag(20)
+                Text("25 seconds").tag(25)
+                Text("30 seconds").tag(30)
+            }
+            .padding(.bottom, 10)
+            NavigationLink(
+                destination: CountingDownView(
+                    secondScreenShown: $secondScreenShown,
+                    timerVal: timerVal
+                ),
+                isActive: $secondScreenShown,
+                label: {Text("Start")}
+            )
+        }
+    }
+}
+
+struct CountingDownView: View {
+    @Binding var secondScreenShown:Bool
+    @State var timerVal = 0
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            if timerVal > 0 {
+                Text("Time remaining")
+                    .font(.system(size: 14))
+                Text("\(timerVal)")
+                    .font(.system(size: 40))
+                    .onAppear() {
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                            if self.timerVal > 0 {
+                                self.timerVal -= 1
+                            }
+                        }
+                    }
+                Text("seconds")
+                    .font(.system(size: 14))
+                    .padding(.bottom, 10)
+                Button(action: {
+                    self.secondScreenShown = false
+                }) {
+                    Text("Cancel").foregroundColor(.red)
+                }
+            } else {
+                Text("Times up!")
+                    .font(.system(size: 30))
+                    .padding(.bottom, 10)
+                Button(action: {
+                    self.secondScreenShown = false
+                }) {
+                    Text("Go back").foregroundColor(.green)
+                }
+            }
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+# Timer App - WatchOS
+
+Simple timer app for the Apple Watch.
+
+![file](assets/app.gif)
